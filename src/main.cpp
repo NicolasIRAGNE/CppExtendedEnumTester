e6fc57164bb2bdc80c3928cdf9956bfce9e6f31a
Nicolas IRAGNE <nicolas.iragne@alyce.fr>
2022-01-04 19:20:08 +0100

    Add names enum

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
  */
 
 #include "capitals.h"
+#include "names.h"
 #include <iostream>
 
 int main()
@@ -18,4 +19,12 @@ int main()
     ExtEnums::capitals::Foreach([](const ExtEnums::capitals& value) {
         std::cout << value.ToString() << std::endl;
     });
+    
+    std::cout << std::endl;
+    std::cout << "Names enum size: " << names::EnumSize() << std::endl;
+    std::cout << "Names:" << std::endl;
+    names::Foreach([](const names& value) {
+        std::cout << value.ToString() << std::endl;
+    });
+    return 0;
 }
